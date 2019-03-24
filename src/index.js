f859c4c0bd97244ba02809a7e9f447849850dceb
7zYBN <frazo4ka@mail.ru>
2019-03-24 23:26:52 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = splitConfigIntoOpenBrackets(bracketsConfig);
+  const closeBrackets = splitConfigIntoCloseBrackets(bracketsConfig);
+  const result = checkStringInCorrect(str, openBrackets, closeBrackets);
+
+  return result;
+}
+
+function splitConfigIntoOpenBrackets(bracketsConfig) {
+  let openBrackets = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+  }
+
+  return openBrackets;
+}
+
+function splitConfigIntoCloseBrackets(bracketsConfig) {
+  let closeBrackets = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    closeBrackets.push(bracketsConfig[i][1]);
+  }
+
+  return closeBrackets;
+}
+
+function checkStringInCorrect(str, openBrackets, closeBrackets) {
+  let array = [];
+  let result = true;
+  for (let i = 0; i < str.length; i++) {
+    if (openBrackets.includes(str[i]) && closeBrackets.includes(str[i])) {
+
+      if (array.includes(str[i])) {
+        let popped = array.pop();
+
+        if (popped !== str[i]) {
+          result = false;
+        }
+
+      } else {
+        array.push(str[i]);
+      }
+
+    } else {
+
+      if (openBrackets.includes(str[i])) {
+        array.push(str[i]);
+      } else {
+        let popped = array.pop();
+
+        if (closeBrackets.indexOf(str[i]) !== openBrackets.indexOf(popped)) {
+          result = false;
+        }
+      }
+    }
+  }
+
+  if (array.length) {
+    result = false;
+  }
+
+  return result;
 }
